35c06f6e97a5bc67d897a476573534f74564f9d0
Ruffel1998 <112846348+Ruffel1998@users.noreply.github.com>
2023-03-16 12:46:42 +0100

    Base files and startupview created

---
+++ Nebuloo_web/Nebuloo_web/src/views/StartupView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="row h-100 bg-primary bg-opacity-75 py-5 text-center text-light">
+        <h1 id="title" class="pt-3 pb-3">Nebuloo</h1>
+        <h4 class="py-3">Tananyag megosztó fórum</h4>
+        <p class="pb-5 pt-3 fs-6">Mert a tudás mindenkit megillet</p>
+    </div>
+        <div class="container bg-light rounded-4 mt-4 mx-auto text-center py-2 px-4 shadow">
+            <div class="row">
+                <p>Nem érted a Viète-formulákat? Kérdezz nyugodtan! Pont hogy érted? Oszdd meg a tudást a többiekkel!</p>
+                <p>Mászd meg a tudás ranglétráját és vállj bölcsé!</p>
+                <p class="text-start">Rangok:</p>
+                <div class="row text-center">
+                    <div class="col-2">
+                        <img src="https://placeholder.pics/svg/35" alt="">
+                        <p>tanuló</p>
+                    </div>
+                    <div class="col-2">
+                        <img src="https://placeholder.pics/svg/35" alt="">
+                        <p>okostojás</p>
+                    </div>
+                    <div class="col-2">
+                        <img src="https://placeholder.pics/svg/35" alt="">
+                        <p>oktató</p>
+                    </div>
+                    <div class="col-2">
+                        <img src="https://placeholder.pics/svg/35" alt="">
+                        <p>tanár</p>
+                    </div>
+                    <div class="col-2">
+                        <img src="https://placeholder.pics/svg/35" alt="">
+                        <p>professzor</p>
+                    </div>
+                    <div class="col-2">
+                        <img src="https://placeholder.pics/svg/35" alt="">
+                        <p>bölcs</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+</template>
+
+<script>
+
+</script>
