d5b56fe9fb274de0eac81a20563dd7f4a32d2896
Ferencneu <2003feco@gmail.com>
2023-03-20 08:46:08 +0100

    CreationForm vue file added

---
+++ Nebuloo_web/Nebuloo_web/src/components/CreationForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <label for="cim" class="form-label">Cím*</label>
+    <input type="text" name="cim" id="cim" class="form-control">
+
+    <label for="leiras" class="form-label">Leírás*</label>
+    <input type="text" name="leiras" id="leiras" class="form-control">
+    <label for="" class="form-label">Kép feltöltése</label>
+
+
+    <label for="cimkek" class="form-label ">Címkék hozzáadása</label>
+    <input type="text" name="cimkek" id="cimkek" class="form-control">
+</template>
+
+<script>
+</script>
