5ec5ce22c6669dc7c250b8e9faaa5384cd352871
Ruffel1998 <112846348+Ruffel1998@users.noreply.github.com>
2023-04-12 12:26:09 +0200

    Modified footer so it adjusts to content

--- resources/components/Footer.vue
+++ resources/components/Footer.vue
@@ -1,20 +1,39 @@
 <template>
-  <div class="container" id="anyad">
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-      <p class="col-md-4 mb-0 text-muted">Nebuloo</p>
-  
-      <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-      </a>
-  
-      <ul class="nav col-md-4 justify-content-end">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Dokumentáció</a></li>
-        <li><router-link class="nav-link text-muted" aria-current="page" to="/create/ticket">Jelentés küldése</router-link></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Forrás</a></li>
-        <li><router-link class="nav-link text-muted" aria-current="page" to="/about">Rólunk</router-link></li>
-        
+  <div class="container d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <footer>
+      <div class="row">
+        <p class="mb-3 text-muted">
+          <router-link class="nav-link text-muted px-2" aria-current="page" to="/">
+            Nebuloo
+          </router-link>
+        </p>
+      </div>
 
-      </ul>
+      <div class="row justify-content-start">
+        <div class="col-sm-6">
+          <div class="row">
+          <div class="col-md-3">
+            <a href="#" class="nav-link px-2 text-muted">Dokumentáció</a>
+          </div>
+          <div class="col-md-3">
+            <router-link class="nav-link text-muted px-2" aria-current="page" to="/create/ticket">Jelentés küldése</router-link>
+
+          </div>
+          <div class="col-md-3">
+            <a href="#" class="nav-link px-2 text-muted">Forrás</a>
+
+          </div>
+          <div class="col-md-3">
+            <router-link class="nav-link text-muted px-2" aria-current="page" to="/about">Rólunk</router-link>
+
+          </div>
+        </div>
+      
+        </div>
+        
+      </div>
+  
+      
     </footer>
   </div>
   </template>
