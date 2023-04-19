f88342c38a2188bd66f3b2ef494668dacd1758e5
Ruffel1998 <112846348+Ruffel1998@users.noreply.github.com>
2023-04-20 00:41:17 +0200

    own comment delete/edit function added

--- resources/components/CommentCard.vue
+++ resources/components/CommentCard.vue
@@ -3,9 +3,22 @@
         <div class="col-sm-1 col-xs-2">
             <user class="text-center" :user="comment.creator"></user> 
         </div>
-        <div class="flex-fill">
-            
-            <p style="margin-left: 5px;">{{comment.message}}</p>
+        <div class="col-xs-9 col-sm-10">
+            <p style="margin-left: 5px;" v-if="!isEditing">{{comment.message}}</p>
+            <textarea id="body" v-model="commentbody" v-if="isEditing" class="form-control" rows="3" cols="10"></textarea>
+
+            <div class="text-end" v-if="MyComment==true">
+                <button class="btn btn-success mx-1" @click="editModeOff()" v-if="isEditing"> 
+                    Szerkeszt
+                </button>
+                <button class="btn btn-info mx-1" @click="editModeOn()" v-if="!isEditing">
+                    Szerkesztés
+                </button>
+                <button class="btn btn-danger mx-1" @click="deleteComment()" v-if="!isEditing"> 
+                    Törlés
+                </button>
+            </div>
+
         </div>
         <div class="col-sm-1 col-xs-1">
             <vote :contentId="comment.id" :voteCount="comment.recieved_votes" :vote="null"></vote>
@@ -15,10 +28,16 @@
 
 <script>
 import Vote from './Vote.vue';
-
+import { NebulooFetch } from '../utils/https.mjs';
 import User from './User.vue';
 
 export default{
+    data(){
+        return{
+            isEditing:false,
+            commentbody:""
+        };
+    },
     components:{
         Vote,
         User
@@ -27,6 +46,45 @@ export default{
         comment: Object,
     },
     mounted(){
+    },
+    methods:{
+        deleteComment(){
+            if (window.confirm("Biztosan törölni szeretné hozzászólását?")) {
+                NebulooFetch.deleteMyComment(this.comment.id)
+                .then(()=>{
+                    alert("Sikeres törlés!");
+                    window.location.reload();
+                });
+            }
+        },
+        editModeOn(){
+            this.isEditing = true;
+        },
+        async editModeOff(){
+            if (window.confirm("Biztosan szerkeszteni szeretné hozzászólását?")) {
+                this.editComment();
+                this.isEditing = false;
+            }
+            else{
+                this.isEditing = false;
+            }
+        },
+        async editComment(){
+            const data = JSON.stringify(this.commentData);
+            var response = (await NebulooFetch.editComment(data,this.comment.id));
+            window.location.reload();
+        },
+    },
+    computed:{
+        MyComment(){
+            const identifier = sessionStorage.getItem('Identifier');
+            return identifier == this.comment.creator.id;
+            },
+            commentData(){
+            return{
+                message:this.commentbody
+            }
+        }
     }
 }
 </script>
