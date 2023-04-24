b57220661a0bce9c1e4e1299b6a5e9c769219c0e
Ruffel1998 <112846348+Ruffel1998@users.noreply.github.com>
2023-04-24 22:34:50 +0200

    Started frontend login tests

---
+++ NebulooWebTest/NebulooWebTest/LoginTestChrome.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace NebulooWebTest
+{
+    public class LoginTestChrome
+    {
+        IWebDriver driver;
+
+        static string baseUrl = "http://localhost:8881/";
+
+        [SetUp]
+        public void Setup()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void SuccessfulLoginTest()
+        {
+            driver.Url = baseUrl + "login";
+            string currentPage = driver.Url;
+            var usernameTextbox = driver.FindElement(By.Name("identifier"));
+            usernameTextbox.SendKeys("Admin");
+
+            var passwordTextbox = driver.FindElement(By.Name("password"));
+            passwordTextbox.SendKeys("admin123");
+
+            var submitButton = driver.FindElement(By.XPath("//*[@id=\"button\"]"));
+            submitButton.Click();
+
+            while (true)
+            {
+                try
+                {
+                    string newPage = driver.Url;
+                    if (currentPage == newPage)
+                    {
+                        Assert.Fail();
+                        break;
+                    }
+                    else
+                    {
+                        Assert.Pass();
+                        break;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            driver.Quit();
+        }
+    }
+}
