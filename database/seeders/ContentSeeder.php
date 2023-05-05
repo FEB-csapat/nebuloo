acffe767d4cad71a17d01fc8a94086af25b74647
Ferencneu <2003feco@gmail.com>
2023-05-05 20:10:22 +0200

    Content seeds added

--- database/seeders/ContentSeeder.php
+++ database/seeders/ContentSeeder.php
@@ -121,6 +121,124 @@ Dosztojevszkij műveiben gyakran az ártatlanok szenvedése és a bűnösök meg
 Dosztojevszkij munkássága az orosz irodalom történetében egyedülálló, és még ma is inspiráló hatással van az olvasókra és a követő írókra. Dosztojevszkij nagy jelentőséggel bír az orosz realizmusban, mivel művei általában az emberi szenvedélyeket és a lelki problémákat elemzik, és a társadalom problémáit is kritikusan vizsgálják.',
         ]);
         
+        Content::factory()->create([
+            'creator_user_id' => 3,
+            'subject_id' => Subject::where('name', 'Fizika')->first()->id,
+            'topic_id' => Topic::where('name', 'Atomfizika')->first()->id,
+            'body' => '#Bevezető
+Az atomfizika az anyag felépítésének és viselkedésének megértésével foglalkozik. Az atomok az anyag alapvető építőkövei, amelyeket a kémiai reakciók és az elektromos erők hatására kombinálnak. Az atomfizika tanulmányozza az atomok felépítését, tulajdonságait és viselkedését, valamint az atommagokat és a radioaktivitást.
+
+### Az atomok felépítése
+Az atomok felépítése egy kicsi, pozitív töltésű atommagból és az azt körülvevő negatív töltésű elektronfelhőből áll. Az atommagot pozitív töltésű protonok és semleges tömegű neutronok alkotják. Az elektronok körülveszik az atommagot és negatív töltésűek. Az elektronok számától függ, hogy milyen elemről van szó. Például az oxigénnek 8 elektronja van az atomjában, míg a hidrogénnek csak 1.
+
+### Az atomok viselkedése
+Az atomok viselkedése különböző tényezőktől függ, például a hőmérséklettől és a nyomástól. Az atomok összeütközhetnek, és ekkor kinetikus energia szabadul fel. Az atomok kémiai reakcióba léphetnek, amikor elektronokat cserélnek vagy kötést alkotnak más atomokkal. Az atommagok szintén kölcsönhatásba léphetnek, amikor az erős nukleáris erő hatására új atommagok keletkeznek.
+
+### Az atommagok és a radioaktivitás
+Az atommagokat protonok és neutronok alkotják, és a protonok száma határozza meg, hogy milyen elemről van szó. Az atommag instabil lehet, és ilyenkor a radioaktív bomlás során átalakul más elemmé vagy egy másik izotóppá. A radioaktív bomlás során radioaktív sugárzás szabadul fel, amely károsíthatja az élő szervezeteket. A radioaktív anyagokat azért használják, mert a sugárzásuk hasznos lehet az orvoslásban és az iparban.
+
+Az atomok és az atommagok megértése segíthet a tudósoknak a környezet védelme, az orvoslás és az ipar fejlesztése, valamint az energiatermelés hatékonyabbá tétele területén.'
+]);
+        Content::factory()->create([
+            'creator_user_id' => 3,
+            'subject_id' => Subject::where('name', 'Informatika')->first()->id,
+            'topic_id' => Topic::where('name', 'Hálózatok')->first()->id,
+            'body' => '# IPv6 Címzés
+Az IPv6 (Internet Protocol version 6) az internetes protokoll új generációja, amely lehetővé teszi az internet címzési rendszerének bővítését és a hálózati eszközök számának növelését. Az IPv6-os címek 128 bitesek, szemben az IPv4-es címek 32 bites változatával, ami azt jelenti, hogy sokkal nagyobb számú egyedi címet lehet velük létrehozni. Az IPv6-os címek előnye, hogy több egyedi címet biztosítanak, és ezáltal a hálózati eszközök nagyobb száma csatlakozhat az internethez.
+
+## Az IPv6-os címek szerkezete
+Az IPv6-os címeket 8 blokkra osztják, és ezeket kettősponttal választják el egymástól. Például: 2001:0db8:85a3:0000:0000:8a2e:0370:7334. A blokkok 16 bitesek, és a blokkokat 4 számjegyre csoportosítják. Ha az egyik blokk értéke nulla, akkor azt rövidített alakban "::"-tel jelölik. Ez a jelölés egyszerűsíti az IPv6-os címek írását és olvasását.
+
+## Az IPv6-os címek típusai
+Az IPv6-os címek többféle típusba sorolhatók. Az egyik típus az ún. unicast cím, amelyet egyetlen eszköz használ az adatátvitelre. A másik típus a multicast cím, amelyet több eszköz használhat az adatátvitelre. A multicast címek lehetővé teszik az adatok hatékonyabb és gyorsabb továbbítását a hálózaton. A harmadik típus a broadcast cím, amelyet minden eszköz használ az adatátvitelre. Az IPv6-os címek további típusai közé tartoznak a loopback címek, az anycast címek és a link-local címek.
+
+## Az IPv6-os címek használata
+Az IPv6-os címeket a hálózati eszközök egyedi azonosítására használják az interneten. Az IPv6-os címet a hálózati eszközök operációs rendszere automatikusan generálja vagy az internetes szolgáltató biztosítja. Az IPv6-os címek használatának előnye, hogy lehetővé teszik a nagyobb eszközszámú hálózatok létrehozását és a hatékonyabb adatátvitelt. Az IPv6-os címek használata azonban nem teljesen elterjedt még az interneten, és az IPv4-es címeket továbbra is használják a legtöbb esetben'
+]);
+        Content::factory()->create([
+            'creator_user_id' => 3,
+            'subject_id' => Subject::where('name', 'Matematika')->first()->id,
+            'topic_id' => Topic::where('name', 'Kombinatorika')->first()->id,
+            'body' => '## Bevezető a valószínűség számításba
+A valószínűség számítása egy matematikai terület, amely arra szolgál, hogy meghatározza az események bekövetkezésének valószínűségét. Ez a terület fontos szerepet játszik a matematikában, az informatikában, a pénzügyekben, a fizikában és sok más területen.
+
+A valószínűség fogalmát gyakran használják a statisztikában, ahol az adatok elemzése során hasznos információkat nyújtanak. A valószínűség alapvetően azt jelenti, hogy mekkora eséllyel fordul elő egy esemény a lehetséges események közül. Ez azt jelenti, hogy ha van egy bizonyos számú lehetőségünk arra, hogy egy adott esemény bekövetkezzen, akkor a valószínűség az, hogy az esemény ténylegesen bekövetkezik.
+
+A valószínűség számítása matematikailag összetett, és sok esetben számítási módszerek, például a kombinatorika és a valószínűségi számítások alkalmazása szükséges. A valószínűségszámítás során fontos megérteni a valószínűségi eloszlásokat és azok tulajdonságait, valamint a valószínűség számításának számos módszerét.
+
+Az alapvető valószínűségi fogalmak és számítási módszerek megértése segíthet abban, hogy jobban megértsük a valószínűségi eseményeket és ezáltal javíthassuk a döntéshozatali folyamatainkat.'
+]);
+        Content::factory()->create([
+            'creator_user_id' => 3,
+            'subject_id' => Subject::where('name', 'Kémia')->first()->id,
+            'topic_id' => Topic::where('name', 'Szerves kémia')->first()->id,
+            'body' => '# Bevezető a szerves kémiába
+A szerver kémiája az a tudomány, amely a számítógépes módszerek és az alkalmazott matematikai modellek alkalmazásával a kémiai reakciók és a molekulák tulajdonságainak vizsgálatával foglalkozik. Az elmúlt évtizedekben a számítógépes kémiában számos új módszer és szoftver fejlődött ki, amelyek lehetővé teszik a molekuláris rendszerek modellezését, azok szerkezetének előrejelzését, valamint a kémiai reakciók mechanizmusainak vizsgálatát.
+
+Az elsődleges célja a számítógépes kémiai modellezésnek, hogy megértsük a kémiai rendszerek működését és azok összetett viselkedését. A modellezés során a molekuláris szerkezeteket és reakciókat matematikai egyenletekkel írják le, amelyek számítógépes szimulációk segítségével jelennek meg.
+
+A számítógépes kémiai modellezés alapjait az anyagok kvantummechanikai leírása adja. Ez a módszer lehetővé teszi az elektronok mozgásának leírását és a molekuláris kötések tulajdonságainak meghatározását. Az elektron szerkezetének meghatározása egy adott molekulában lehetővé teszi a molekula számos tulajdonságának előrejelzését, például a dipólusmomentumot, az ionizációs energiát, a reakció sebességét és az abszorpciós spektrumot.
+
+Azonban a kvantummechanikai módszerek nagyon költségesek, és korlátozottak az alkalmazható molekulák számára. Emiatt a számítógépes kémiai modellezés során gyakran alkalmaznak klasszikus módszereket is, mint például a molekuláris dinamikát, amely lehetővé teszi a molekulák dinamikus viselkedésének szimulálását.
+
+A szerver kémia egy másik fontos alkalmazási területe az adatbányászat a kémiai adatokból. Ez a módszer lehetővé teszi a nagy mennyiségű adatok elemzését, és azonosítását, mint például a kémiai szerkezetek, tulajdonságok, aktivitás és reakciók eredményei.
+
+A szerver kémiában alkalmazott számítógépes módszerek és programok széles körben alkalmazhatók a gyógyszertervezés, az anyagfejlesztés és az ipari folyamatok optimalizálása'
+]);
+        Content::factory()->create([
+            'creator_user_id' => 3,
+            'subject_id' => Subject::where('name', 'Történelem')->first()->id,
+            'topic_id' => Topic::where('name', '2. világháború')->first()->id,
+            'body' => '# Sztálingrádi csata
+A Sztálingrádi csata a második világháború egyik legnagyobb és legvéresebb ütközete volt, amely a náci Németország és a Szovjetunió között zajlott. A csata során a két hadsereg több mint öt hónapig harcolt egymással, és a szovjetek végül győztek. A Sztálingrádi csata stratégiai fontosságú volt a háború kimenetele szempontjából, és jelentős hatást gyakorolt a további harcok menetére.
+## A Sztálingrádi csata előzményei
+A második világháborúban a németek 1941-ben indítottak támadást a Szovjetunió ellen, és a következő évben előrenyomultak a Don folyó völgyébe. A Sztálingrád városát átszelő Volga folyó keresztezése a németek számára stratégiai fontosságú volt, mert az ellenséges erők egy részét vissza tudták volna tartani a Volga túloldalán, és így könnyebben haladhattak volna az orosz területen.
+
+## A Sztálingrádi csata menete
+A Sztálingrádi csata 1942 nyarán kezdődött, amikor a németek támadást indítottak a város ellen. A csata azonban gyorsan elakadt, mert a szovjetek keményen ellenálltak és felkészültek a védelemre. A városban zajló harcoktól a lakosság súlyosan megszenvedett, és több ezer ember vesztette életét. A csata során mindkét oldal számára óriási veszteségek voltak, és a németek nem tudták elfoglalni a várost.
+
+A csata során a szovjeteknek sikerült visszavonulniuk a Don folyó túloldalára, majd támadást indítaniuk a németek ellen. A szovjet erők fokozatosan visszavették a kezdeményezést, és lassan haladtak előre. A csata legvéresebb szakaszai november és december között zajlottak, amikor a szovjetek előrenyomultak a városban, és végül sikerült elfoglalniuk a teljes területet.
+
+## A Sztálingrádi csata következményei
+A Sztálingrádi csata stratégiai fontosságú volt a háború kimenetele szempontjából, mert megakadályozta a németek előrenyomulását a Szovjetunió területén. A csata után a szovjetek fokozatosan előnybe kerültek a háborúban, és a németek előretörésének megakadályozása miatt elvesztették a lehetőséget, hogy kiterjesszék területi uralmukat a szovjetek fölött. A Sztálingrádi csata után a szovjetek elkezdtek áttérni a támadó hadviselésre, és a kezdeményezés visszavételével fokozatosan visszaverték a német erőket. A csata szimbolikus jelentőséggel is bír, hiszen az egész világ számára világossá vált, hogy a Szovjetunió nem lesz könnyen legyőzhető, és hogy a háború végkimenetele még nem eldöntött. A Sztálingrádi csata a második világháború egyik legnagyobb tragédiája volt, és azóta is emlékezetes eseményként él a történelemkönyvekben.'
+        ]);
+        Content::factory()->create([
+            'creator_user_id' => 3,
+            'subject_id' => Subject::where('name', 'Történelem')->first()->id,
+            'topic_id' => Topic::where('name', '2. világháború')->first()->id,
+            'body' => '# A Marinot vonal
+A Marinot vonal egy híres, de sikertelen védelmi vonal, amelyet a francia hadsereg épített a német hadsereg ellen az első világháború során. A vonalat azért építették, hogy megakadályozzák a németek előretörését Franciaországban.
+
+A vonal építése 1915-ben kezdődött, és az első vonal az Ardennes erdőkön átvezetett és kiterjedt a Vosges-hegységig. A második vonal a belső területeken helyezkedett el, és azzal a céllal építették, hogy megakadályozzák a német hadsereg további előretörését, ha sikerült áttörni az első vonalat.
+
+Azonban a vonal építése hosszú és költséges volt, és a német hadseregnek sikerült áttörnie a vonalat a Verdun-nál 1916 februárjában. A Verdun-i csata a legnagyobb és legsúlyosabb csata volt az első világháborúban, és nagyon nagy veszteségeket okozott mindkét oldalon.
+
+A Marinot vonal építése nem bizonyult hatékonynak, mert a német hadsereg már ekkor fejlett technikával rendelkezett, amely lehetővé tette számukra, hogy áttörjék a vonalat. A vonal nem volt eléggé erős ahhoz, hogy megállítsa a modern katonai technológiák előretörését.
+
+Ennek ellenére a Marinot vonal fontos tanulságot jelentett a hadviselés történetében. Az építése arra kényszerítette a német hadsereget, hogy több erőforrást és időt fordítson az áttörésre, ami csökkentette az előnyüket a háborúban. Ezenkívül a vonal építése és a Verdun-i csata azért is fontos volt, mert az első világháborúban a hadviselés jelentősen megváltozott, és egyre inkább a modern technológia felhasználására és az ipari erőforrásokra támaszkodott.
+
+Összességében a Marinot vonal bár sikertelen volt, fontos szerepet játszott az első világháborúban, és számos tanulságot nyújtott a hadviselésre vonatkozóan a jövő generációi számára.'
+]);
+        Content::factory()->create([
+            'creator_user_id' => 3,
+            'subject_id' => Subject::where('name', 'Történelem')->first()->id,
+            'topic_id' => Topic::where('name', '1. világháború')->first()->id,
+            'body' => '# Trianon
+A trianoni békeszerződés az első világháború után létrejött békeszerződés, amelyet 1920-ban írtak alá a győztes szövetséges hatalmak és a legyőzött Magyarország között. A szerződés Magyarországot és az Osztrák-Magyar Monarchiát érintette, és a világháborút lezáró béketárgyalások keretében jött létre.
+
+## A trianoni békeszerződés előzményei
+Az első világháborút követően a győztes szövetséges hatalmak összegyűltek, hogy megvitassák a békeszerződés feltételeit. A béketárgyalások során Magyarország kénytelen volt elfogadni az ország területének jelentős csökkentését, a hadsereg korlátozását és az ország gazdasági önállóságának megszüntetését.
+            
+## A trianoni békeszerződés tartalma
+A trianoni békeszerződés értelmében Magyarország elveszítette területének nagy részét, és csak az ország jelenlegi határai maradtak meg. Az ország területének mintegy 72%-a elcsatolásra került, és az ország kisebbségi területeire számos új állam jött létre, mint például Csehszlovákia, Románia és Jugoszlávia.
+
+Az ország gazdasági önállóságának megszüntetése értelmében Magyarország elvesztette a korábbi gazdasági előnyeit is, és az ország gazdasági helyzete jelentősen romlott. A trianoni békeszerződés következtében Magyarország elveszítette a hadseregét, és csupán 35 000 főt tartottak fenn a hadseregben.
+
+## A trianoni békeszerződés következményei
+A trianoni békeszerződés jelentős hatást gyakorolt Magyarországra és az ország történelmére. Az ország elvesztette a korábbi területi és gazdasági előnyeit, és a gazdaság helyzete hosszú ideig nehéz helyzetben volt. Az országban élő kisebbségek számos problémával szembesültek, és az ország egész lakosságát érintette a trianoni békeszerződés következményeinek nehézsége.
+
+A trianoni békeszerződés azonban nem csak Magyarországot érintette, hanem az egész régiót is. A békeszerződés következtében új államok jöttek létre,amelyek között az etnikai, gazdasági és politikai problémák folyamatosan jelen voltak. A trianoni békeszerződés jelentőségét nehéz túlbecsülni, hiszen az ország történelmének egyik legtragikusabb eseménye volt, és az ország sorsát hosszú időre meghatározta. A trianoni békeszerződés napjára azóta is emlékeznek, és az ország újrarendezésének nehéz korszaka egy olyan emlékezetes pillanatává vált, amely az ország történelmében mindig megmarad.'
+]);
         Content::factory()->count(20)->create();
     }
 }
